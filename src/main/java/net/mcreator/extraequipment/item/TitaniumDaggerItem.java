412850cfb669666119dc837fb313664c39071144
FadeXI <riotbtw@GMAIL.COM>
2020-10-23 16:53:25 -0400

    Dagger

---
+++ src/main/java/net/mcreator/extraequipment/item/TitaniumDaggerItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.extraequipment.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.extraequipment.itemgroup.ExtraequipmentItemGroup;
+import net.mcreator.extraequipment.ExtraEquipmentModElements;
+
+@ExtraEquipmentModElements.ModElement.Tag
+public class TitaniumDaggerItem extends ExtraEquipmentModElements.ModElement {
+	@ObjectHolder("extra_equipment:titanium_dagger")
+	public static final Item block = null;
+	public TitaniumDaggerItem(ExtraEquipmentModElements instance) {
+		super(instance, 14);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 10;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 4f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(TitaniumingotItem.block, (int) (1)));
+			}
+		}, 3, -3f, new Item.Properties().group(ExtraequipmentItemGroup.tab)) {
+		}.setRegistryName("titanium_dagger"));
+	}
+}
