39d3a1972ab72af7f96b6e833309ad296318c549
FadeXI <riotbtw@GMAIL.COM>
2020-10-23 18:05:19 -0400

    Endshard

---
+++ src/main/java/net/mcreator/extraequipment/item/EndshardItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.extraequipment.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.extraequipment.itemgroup.ExtraequipmentItemGroup;
+import net.mcreator.extraequipment.ExtraEquipmentModElements;
+
+@ExtraEquipmentModElements.ModElement.Tag
+public class EndshardItem extends ExtraEquipmentModElements.ModElement {
+	@ObjectHolder("extra_equipment:endshard")
+	public static final Item block = null;
+	public EndshardItem(ExtraEquipmentModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ExtraequipmentItemGroup.tab).maxStackSize(64));
+			setRegistryName("endshard");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
